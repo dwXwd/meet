beba1ea615f3c0b079bec1ecd37c5f776a6949f2
Your Name <dennisweissert.wd@gmail.com>
2022-05-25 17:10:45 +0200

    updated icons and api.js

--- src/api.js
+++ src/api.js
@@ -99,6 +99,18 @@ export const getEvents = async () => {
       return data ? JSON.parse(data).events : [];
   }
 
+  if (result.data) {
+    var locations = extractLocations(result.data.events);
+    localStorage.setItem('lastEvents', JSON.stringify(result.data));
+    localStorage.setItem('locations', JSON.stringify(locations));
+  }
+
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data?JSON.parse(events).events:[];;
+  }
+
   const token = await getAccessToken();
 
   if (token) {
