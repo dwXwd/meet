bc9061be9f7befe20796d028cef8f0276bf8abfa
Your Name <dennisweissert.wd@gmail.com>
2022-05-26 00:08:34 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -111,9 +111,10 @@ export const getEvents = async () => {
           var locations = extractLocations(result.data.events);
           localStorage.setItem("lastEvents", JSON.stringify(result.data));
           localStorage.setItem("locations", JSON.stringify(locations));
-      }
+
       NProgress.done();
       return result.data.events;
+    }
   }
 
 };
